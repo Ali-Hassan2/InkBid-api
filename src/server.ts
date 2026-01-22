68e279e4b9a0e8510830a078b467740497249759
Ali Hassan <alihassan26032004@gmail.com>
2026-01-22 21:13:52 +0500

    cors/prod-code setup

--- src/server.ts
+++ src/server.ts
@@ -3,22 +3,39 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
 import colors from 'colors';
-import cookieParser from 'cookie-parser'
+import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
+import { randomUUID } from 'crypto';
+import rateLimit from 'express-rate-limit';
+
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 8123;
+const isProduction = process.env.NODE_ENV === 'production';
 
 app.use(express.json());
-app.use(cors());
-app.use(morgan('dev'));
-app.use()
-
-let Server;
+app.use(
+  cors({
+    origin: '*',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Cookie', 'Authorization'],
+  }),
+);
+app.use(helmet());
+app.use(morgan(isProduction ? 'combined' : 'dev'));
+app.use(cookieParser());
+app.use(
+    rateLimit({
+        
+    })
+)
+let Server: any;
 const start_server = () => {
   return new Promise((resolve, reject) => {
     try {
-      app.listen(port, () => {
+      Server = app.listen(port, () => {
         console.log(colors.bgGreen(`Server is running at port: ${port}`));
       });
       resolve(Server);
